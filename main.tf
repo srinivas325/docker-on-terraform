13e910d85c6dd695c5c719b344eb0382b4de85cb
srinivas325 <59291856+srinivas325@users.noreply.github.com>
2021-07-15 15:33:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.14.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+
 resource "docker_container" "hello_world_app" {
   image = "hello_world_app:latest"
   name  = "hello_world_app"
